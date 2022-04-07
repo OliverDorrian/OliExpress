e9db7fdef1e0e25002da66ffa9c76790ded16b78
OliverDorrian <dorrianolfc@gmail.com>
2022-04-07 14:55:55 +0100

    Final commit

--- shopOnline.php
+++ shopOnline.php
@@ -1,9 +1,59 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "root", "oliexpress");
+$conn = mysqli_connect("localhost", "root", "root", "items");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$sql = "DROP TABLE item;";
+mysqli_query($conn, $sql);
+
+$sql = "DROP TABLE category;";
+mysqli_query($conn, $sql);
+
+$sql = "CREATE TABLE `category` (
+  `CatID` int NOT NULL,
+  `CatName` text NOT NULL
+);";
+mysqli_query($conn, $sql);
+
+
+$sql = "INSERT INTO `category` (`CatID`, `CatName`) VALUES
+(1, 'Men\'s Casual Clothing'),
+(2, 'Men\'s Formal Clothing');";
+mysqli_query($conn, $sql);
+
+
+$sql = "CREATE TABLE `item` (
+  `ItemID` int NOT NULL,
+  `CatID` int NOT NULL,
+  `urlOfImg` tinytext NOT NULL,
+  `Name` text NOT NULL,
+  `Price` float NOT NULL
+);";
+mysqli_query($conn, $sql);
+
+$sql = "INSERT INTO `item` (`ItemID`, `CatID`, `urlOfImg`, `Name`, `Price`) VALUES
+(1, 1, 'https://ae01.alicdn.com/kf/Sbd75d011f2094f02ae0fd35af48c3b450/2022-Spring-Autumn-Japanese-Streetwear-Black-Casual-Lapel-Jacket-Men-Clothing-Korean-Couple-Coat-Harajuku-Loose.jpg_Q90.jpg_.webp', 'Dickies Men\'s Blue Jacket', 20),
+(2, 1, 'https://ae01.alicdn.com/kf/H93f83822dbe7431a95ac491746338152x/100-Cotton-Pullover-Men-Sweater-2021-Autumn-Winter-Man-Clothes-Jersey-Sueter-Hombre-Pull-Homme-Sweter.jpg_Q90.jpg_.webp', 'Blue Men\'s Jumper', 28),
+(3, 2, 'https://ae01.alicdn.com/kf/H55cde1df7c9644ee9db7e963962c6c7ah/2022-Mens-Loose-Style-Jacket-Motorcycle-Biker-Leather-Jacket-Men-Fashion-Leather-Coats-Male-Bomber-Jacket.jpg_Q90.jpg_.webp', 'Men\'s Black jacket', 27),
+(4, 1, 'https://ae01.alicdn.com/kf/Hd0a554ee7705423e8427e6c32765ddcbx/IEFB-Men-s-Clothing-Spring-New-Oversize-Coat-Korean-Trend-Loose-Casual-PU-Leather-Jacket-Coat.jpg_Q90.jpg_.webp', 'Men\'s Classic Leather', 35),
+(5, 1, 'https://ae01.alicdn.com/kf/H4c959635443b48f3a818c2e8d256af3bZ/Autumn-Plaid-Jacket-Men-Fashion-Retro-Pocket-Casual-Jacket-Men-Streetwear-Korean-Loose-Lapel-Coat-Mens.jpg_Q90.jpg_.webp', 'Men\'s Warm Jacket', 29),
+(6, 1, 'https://ae01.alicdn.com/kf/H09c89d3492ec49d09a789dcc0d2dd9c3I/IEFB-Male-Pullover-Sweater-Autumn-Winter-Thickened-Lapel-Zipper-Ins-Casual-Knitted-Tops-Grey-Black-Korean.jpg_Q90.jpg_.webp', 'Men\'s Black Sweater ', 34),
+(7, 1, 'https://ae01.alicdn.com/kf/Sa8c9858b58164888b650fc0d1a1ed30dW/Cardigan-Men-Autumn-Japanese-Solid-Sweater-High-Street-Loose-Trendy-All-match-O-neck-Knitting-Simple.jpg_Q90.jpg_.webp', 'Men\'s Blue Cardigan', 12),
+(8, 1, 'https://ae01.alicdn.com/kf/S405f36fb8eb4409aa25262580f022647f/Privathinker-Half-Sleeve-Men-Solid-Shirts-Summer-Casual-Oversize-Blouses-White-Fashion-Male-Cardigan-Vintage-Korean.jpg_Q90.jpg_.webp', 'Men\'s Black Shirt', 10.75),
+(9, 1, 'https://ae01.alicdn.com/kf/Sf7b3207fc9c34920a834486cd9f2c4393/2022-Spring-Autumn-Japanese-Streetwear-Black-Casual-Lapel-Jacket-Men-Clothing-Korean-Couple-Coat-Harajuku-Loose.jpg_Q90.jpg_.webp', 'Men\'s Black Jacket', 30.27),
+(10, 1, 'https://ae01.alicdn.com/kf/H93320fa276de42c5a1ba71b537c025d03/Korean-Fashion-Turtleneck-Sweater-Men-Streetwear-Men-High-Neck-Knitted-Sweaters-Men-Fashion-Clothing-2021-Trend.jpg_Q90.jpg_.webp', 'Men\'s White Jumper', 19.35),
+(11, 2, 'https://ae01.alicdn.com/kf/H5871c420676b448aad218eda25ea60f99/GODLIKEU-Blazers-Men-Casual-British-Trendy-Loose-Korean-Suit-Tops-Male-Vintage-Jacket-Streetwear.jpg_Q90.jpg_.webp', 'Men\'s White Blazer', 15.38),
+(12, 2, 'https://ae01.alicdn.com/kf/Hbf3ce162d50548cb8ea7374f5a6a1f051/2021-Brand-New-Autumn-Trench-Korean-Men-s-Fashion-Overcoat-for-Male-Long-Windbreaker-Streetwear-Men.jpg_Q90.jpg_.webp', 'Men\'s Autumn Trench', 24.73),
+(13, 2, 'https://ae01.alicdn.com/kf/Hbed8cb1e98e04be8a51fb8c6966f3ecec/Oversized-Men-s-Business-Plaid-Long-sleeved-Shirt-Classic-Formal-Wear-Design-Boss-Quality-Work-Clothes.jpg_Q90.jpg_.webp', 'Men\'s Business Shirt', 11.49),
+(14, 2, 'https://ae01.alicdn.com/kf/H2bf7187c47044b159b5803b8744a338df/Men-s-Western-style-Trousers-Male-Loose-Leisure-Casual-Pants-Business-Design-Cotton-Suit-Pants-Formal.jpg_Q90.jpg_.webp', 'Men\'s Black Trousers', 20.24),
+(15, 2, 'https://ae01.alicdn.com/kf/Se9834d224add4cb6a5cb19c46f89c614k/Pleated-Thin-Mens-Suit-Pants-Black-White-Slim-White-Dress-Pants-Men-Trousers-Office-Business-Korea.jpg_Q90.jpg_.webp', 'Men\'s Suit Pants ', 13.21),
+(16, 2, 'https://ae01.alicdn.com/kf/Sdf99d1959a6e465faa400acd315236cef/Korean-Fashion-Pants-Men-Casual-Slim-Fit-Work-Tousers-9-Part-Ankle-Length-Skinny-Pants-Men.jpg_Q90.jpg_.webp', 'Pants Men Casua', 20.07),
+(17, 2, 'https://ae01.alicdn.com/kf/H2c1d70a77070450f9099c363481334f0x/HYBSKR-Summer-Men-Shirt-Short-Sleeve-Solid-Color-Shirts-For-Man-Vintage-Harajuku-Casual-Oversized-Blouses.jpg_Q90.jpg_.webp', 'Men\'s Short Sleave', 11.24),
+(18, 2, 'https://ae01.alicdn.com/kf/Hfb9f49061ad44350b165f0eee565f64dI/Privathinker-Pleated-Straight-Pants-Men-s-Elastic-Waist-Casual-Pants-Men-Streetwear-Loose-Ice-Silk-Trousers.jpg_Q90.jpg_.webp', 'Men\'s Elatic Pants', 11.57),
+(19, 1, 'https://ae01.alicdn.com/kf/Ha4aff5c3f4a446fdac2aeddc29689ccao/Fw21-new-men-s-polar-fleece-half-zipper-designer-brand-Jerry-Lorenzo-rubber-print-letters-hip.jpg_Q90.jpg_.webp', 'Men\'s Polar Fleece', 27.38),
+(20, 2, 'https://ae01.alicdn.com/kf/H69a5036e23f540fbb97b2f7e68e85f45K/2022-autumn-pocket-super-hot-tooling-coat-fashion-casual-men-s-loose-retro-Japanese-jacket-M.jpg_Q90.jpg_.webp', 'Men\'s Slim Jacket', 43.24);";
+mysqli_query($conn, $sql);
 
 function genItems($conn){
   $sql = "SELECT * FROM `category`";
